8d9abff538bf192b87e0d6309e8e78e03274dace
Krishna Bansal <krishna.web18@gmail.com>
2023-03-26 16:07:23 +0530

    init change

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const app = express();
 const home = require('./routes/home');
 const port= process.env.PORT || 8080;
 const dotenv = require('dotenv');
+const cors = require('cors');
+const morgan= require('morgan');
+
 dotenv.config();
 app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
 app.use('/api/v1',home);
 
 app.get('/',(req,res)=>{
